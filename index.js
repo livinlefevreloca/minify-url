74d8ee1f9246e003575b18a90d04f8a6b75253ca
Adam Lefevre <lefevrad39531@gmail.com>
2017-11-14 20:36:51 -0600

    most up to date version

--- index.js
+++ index.js
@@ -1,45 +1,60 @@
 var express = require('express');
 var mongo = require('mongodb').MongoClient;
 var validurl = require('check-valid-url');
-var crypto = require('crypto');
-
+var http = require('http');
 
 var dbUrl = "mongodb://localhost:27017/minifyurl";
-var uri = "http://localhost:8000/";
+var uri = "http://localhost:1337/";
 var app = express();
 
 
+app.all("*", function(req, res, next) {
+  //res.setHeader(200, { "Content-Type": "text/plain" });
+  next();
+});
 
-app.get('/:id' function(req,res){
-	var id = req.param.id;
-	if(Number.isInteger(parseInt(id)){
+
+app.get('/:id', function(req,res){
+	var id = req.params.id;
+
+	if(Number.isInteger(parseInt(id))){
 		mongo.connect(dbUrl,function(err,db){
 		var urls = db.collection('urls');
 		urls.find({_id: id}).toArray(function(err,data){
 			if(err) {console.log(err); throw err;}
-			res.writeHead(302,{'Location': data[0].url});
-			res.end()
+			else if(data[0] === undefined){
+			res.setHeader(200, { "Content-Type": "application/JSON" });
+			res.json({'error': "url not found in DB"});
+			res.end();
 			db.close();
+			}
+			else{
+			res.redirect(data[0].url)
+			res.end();
+			db.close();
+		}
 		});
 	});
 
 			}
 	else {
-		res.json({'error': "the Url was not found in the DB"});
+		res.setHeader(200, { "Content-Type": "application/JSON" });
+		res.json({'error': "the Url given was incorrect"});
 		res.end()
 	}
 
 
-			)})
+});
 
-app.get('new/:url' function(req, res){
-	var url = req.param.url;
+app.get('/new/*', function(req, res){
+	var url = req.params[0];
+	console.log(url);
 	if(validurl.isUrl(url)){
 		mongo.connect(dbUrl,function(err,db){
 			if(err){console.log(err); throw err;}
 			var urls = db.collection('urls');
 			urls.find().toArray(function(err, data){
-				var key = Date.now().toString().splice(-4);
+				var key = Date.now().toString().slice(-4);
 				var check = data.indexOf(key);
 				while(check >= 0){
 				key += 1;
@@ -48,13 +63,23 @@ app.get('new/:url' function(req, res){
 			urls.insert({'_id': key, 'url': url}, function(err,doc){
 				if(err){console.log(err); throw err;}
 				console.log(doc + " was added to urls");
+				res.end("your new url is " + uri + key)
 				db.close();
+
 			});
 
 
 		});
 	});
 	}
-	else{res.json({'error': 'The url supplied is not a valid url'});
+	else{
+		res.setHeader(200, { "Content-Type": "application/JSON" });
+		res.json({'error': 'The url supplied is not a valid url'});
 }
 });
+
+app.get("*", function(request, response) {
+  response.end("404");
+});
+
+http.createServer(app).listen(1337);
